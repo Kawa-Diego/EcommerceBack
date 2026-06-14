a64b8c756f23c0f9bd9d1f33dfcfd39c72fbfbe2
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-06-14 16:22:25 -0300

    feat: UserRepo has the logic to communicate with database and login logic

--- src/main/java/com/ecommerce/demo/repository/UserRepository.java
+++ src/main/java/com/ecommerce/demo/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.ecommerce.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 import java.util.UUID;
 import com.ecommerce.demo.model.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    
+    Optional<User> findByEmail(String email);
 }
