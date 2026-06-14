6bac2f53ab6446b092ee49e0b8d8a6de93a75882
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-06-14 16:20:10 -0300

    feat: UserController has now the functionallity to comunicate with records. Authenticating login and register from user.

--- src/main/java/com/ecommerce/demo/controller/UserController.java
+++ src/main/java/com/ecommerce/demo/controller/UserController.java
@@ -1,31 +1,27 @@
 package com.ecommerce.demo.controller;
 
-import com.sun.net.httpserver.Request;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value = "/api/users")
+@RequestMapping(value = "/api")
 public class UserController {
 
-    @GetMapping
-    public String apiList() { 
-        return "api list";
-    }
-
     @GetMapping("/user")
-    public String getAllUsers() {
-        return "All Users";
+    public ResponseEntity<String> getAllUsers() {
+        return ResponseEntity.ok("sucesso");
     }
 
-    @GetMapping("/{id}")
-    public String getSingleUser() {
-        return "Hello World!";
-    }
-
-//    @PostMapping("/createUser")
-//    public String createUser(Request request){
-//
+//    @GetMapping("/user/{id}")]
+//    public ResponseEntity(String) getUserById(@PathVariable("id") Long id) {
+//        return userService.findById(id)
+//                .map(user -> ResponseEntity.ok(user))
+//                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 //    }
+
+
 
 }
