2ca9e866dcb53c9f28e547b30d9f7f39cf36f60d
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-06-14 10:57:27 -0300

    chore removed items from repo

--- src/main/resources/db/migration/V4__create-Cart-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "Cart" (
-    id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "userId"        UUID NOT NULL UNIQUE REFERENCES "User"(id) ON DELETE CASCADE,
-    "createdAt"     TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    "updatedAt"     TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
--- src/main/resources/db/migration/V7__create-OrderItem-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "OrderItem" (
-    id                  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "orderId"           UUID NOT NULL REFERENCES "Order"(id) ON DELETE CASCADE,
-    "productId"         UUID NOT NULL REFERENCES "Product"(id),
-    quantity            INTEGER NOT NULL,
-    "unitPriceCents"    INTEGER NOT NULL,
-    "refundRequestedAt" TIMESTAMPTZ,
-    "refundConfirmedAt" TIMESTAMPTZ
-);
