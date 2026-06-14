835a5919702286563ecb4f4d0dfcc224e8ab1bad
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-06-14 16:25:21 -0300

    feat: adding HttpSecurity and its filters. It may do checks for /login and /register authentication.

--- src/main/java/com/ecommerce/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/ecommerce/demo/security/CustomUserDetailsService.java
@@ -1,5 +1,28 @@
 package com.ecommerce.demo.security;
 
-public class CustomUserDetailsService {
-    
+import com.ecommerce.demo.model.User;
+import com.ecommerce.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails
+                .User(user.getEmail(),
+                user.getPassword(),
+                new ArrayList<>());
+    }
 }
