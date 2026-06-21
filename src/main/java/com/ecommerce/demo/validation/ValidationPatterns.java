8aadb8be207ea95a026e50798935b9c0d602f5c8
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-06-21 06:21:50 -0300

    chore: email patterns. Validation to create strong email

---
+++ src/main/java/com/ecommerce/demo/validation/ValidationPatterns.java
@@ -0,0 +1,10 @@
+package com.ecommerce.demo.validation;
+
+public final class ValidationPatterns {
+
+    public static final String EMAIL =
+            "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+    private ValidationPatterns() {
+    }
+}
